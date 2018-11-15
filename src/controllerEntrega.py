1c76177c9ca58ceeff2fc95d93d0492e4e45974f
Javier Boned <javiboned@gmail.com>
2018-11-15 17:12:45 +0100

    entreguita

--- src/controllerEntrega.py
+++ src/controllerEntrega.py
@@ -5,6 +5,20 @@ import comic
 
 class ControllerEntrega(object):
     
-    wsdl = 'http://localhost:8080/HumilArt/ComicWebService?WSDL'
+    wsdl = 'http://localhost:8080/HumilArt/EntregaWebService?WSDL'
     client = Client(wsdl)
     client.options.cache.clear()
+    
+    def findEntregaById(self, idComic):
+        c = self.client.service.findEntregaById();
+        if c:
+            cam = comic.Comic(c['idComic'],c['nombre'],c['descripcion'],c['fechaCreacion'])
+            return cam
+        else:
+            return None
+
+    def addComic(self,nombre,descripcion):
+        self.client.service.addComic(nombre,descripcion)
+    def deleteComic(self,comic):
+        self.client.service.remove(comic)
+    def editComic(self, comic, nuevoNombre,nuevaDescripcion):
---
+++ src/entrega.py
@@ -0,0 +1,19 @@
+
+class Entrega(object):
+    def __init__(self, idEntrega, nombre, archivo, fechaCreacion,idComic):
+        self.idEntrega = idEntrega
+        self.nombre = nombre
+        self.archivo = archivo
+        self.fechaCreacion = fechaCreacion
+        self.idComic = idComic
+        
+    def comoArray(self):
+        return {
+            'idEntrega':str(self.idEntrega),
+            'nombre':self.nombre,
+            'archivo':self.archivo,
+            'fechaCreacion':self.fechaCreacion,
+            'idComic':self.idComic
+            }
+    def __str__(self):
+        return 'id: '+str(self.idComic)+' nombre '+self.nombre+' fechaCreacion: '+self.descripcion
